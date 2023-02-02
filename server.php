05f3076968fa6cae4c4647f2d2a01d6011aa3840
Saraesp <saraesposito145@gmail.com>
2023-02-02 10:24:09 +0100

    correzione server.php

--- server.php
+++ server.php
@@ -6,17 +6,17 @@
     if(isset($_POST['todo'])){
         $todo_item = $_POST['name'];
 
-        $todo_array = {
-            "name" => $todo_item;
-            "done" => false;
-        }
+        $todo_array = [
+            "name" => $todo_item,
+            "done" => false,
+        ];
 
         $list_todo[] = $todo_array;
 
         file_get_contents('list-todo.json', json_encode($list_todo));
     }
 
-    header('Content-Type': 'application/json');
+    header('Content-Type: application/json');
 
     echo json_encode($list_todo);
   
